e5d42a9ecbcee8c92c298995a17a19fac54a2d1e
lgoldfarb33 <leorg8@gmail.com>
2021-09-21 20:45:58 -0400

    Push for assignment 4

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -30,6 +30,20 @@ computed: {
       },
 
     methods:{
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.person = parsedJson.results[0]
+                console.log("C");
+            })
+            .catch( err => {
+                console.error(err)
+            })
+
+            console.log("B");
+        }
 
       },
 
